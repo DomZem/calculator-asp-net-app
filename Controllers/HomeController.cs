676a1341fdebb43832205f0a11997528653c5b5f
Dominik <dominik.zemlik.dev@gmail.com>
2023-10-02 19:16:26 +0200

    Complete calculator

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,34 @@ namespace calculator_asp_net_app.Controllers
             return View();
         }
 
+        public IActionResult Calculator(Calculator calculator)
+        {
+
+            switch(calculator.Operation)
+            {
+                case "+": 
+                    ViewBag.Result = calculator.FirstNumber + calculator.SecondNumber;
+                    break;
+                case "-":
+                    ViewBag.Result = calculator.FirstNumber - calculator.SecondNumber;
+                    break;
+                case "*":
+                    ViewBag.Result = calculator.FirstNumber * calculator.SecondNumber;
+                    break;
+                case "/":
+                    if (calculator.SecondNumber == 0)
+                        ViewBag.Result = "You cannot divide by zero!!!";
+                    else 
+                        ViewBag.Result = calculator.FirstNumber / calculator.SecondNumber;
+                    break;
+                default:
+                    ViewBag.Result = "Unexpected error. Try maybe later!";
+                    break;
+            }
+
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Models/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace calculator_asp_net_app.Models
+{
+    public class Calculator
+    {
+        public int Id { get; set; }
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public string Operation { get; set; }
+    }
+}
